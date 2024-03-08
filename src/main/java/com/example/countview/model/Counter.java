b4136fdd0d3301f20d81e4d00b94ebbef9e830a8
thanhdung26031994 <thanhdung26031994@gmail.com>
2024-03-08 10:52:25 +0700

    count view

--- src/main/java/com/example/countview/model/Counter.java
+++ src/main/java/com/example/countview/model/Counter.java
@@ -1,4 +1,24 @@
 package com.example.countview.model;
 
-public class Count {
+public class Counter {
+    private int count;
+
+    public Counter() {
+    }
+
+    public Counter(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void increment() {
+        count++;
+    }
 }
